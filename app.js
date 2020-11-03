8951485422ab248f0a555add2696f363dde1e923
myndert <ricky_0321@hotmail.com>
2020-11-03 13:12:54 -0600

    First commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+ app.get('/:num1/:num2', (req, res, next) => {
+  const num1 = parseInt(req.params.num1, 10);
+  if (isNaN(num1)) { return res.status(400).send('Not an integer'); }
+  const num2 = parseInt(req.params.num2, 10);
+  if (isNaN(num2)) { return res.status(400).send('Not an integer'); }
+  const result = num1 + num2;
+  return res.json({sum: result.toString()});
+ });
+
+app.get('/:num', (req, res, next) => {
+  const parsed = parseInt(req.params.num, 10);
+  if (isNaN(parsed)) { return res.status(400).send('Not an integer'); }
+  const result = parsed + 10;
+  return res.json({sum: result.toString()});
+ });
+
+app.listen(3000, () => {
+ console.log('Server running on port 3000');
+});
